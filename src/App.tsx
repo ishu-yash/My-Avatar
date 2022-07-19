43be6efa4873cb17cf65fd5bd1d3c05f0a8ffefe
ishu-yash <sheshtrivedi8046@gmail.com>
2022-07-20 00:45:50 +0530

    [ADD] basic avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AvatarView from "@sarge/avatar-view";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AvatarView
+        rotateAvatar
+        eyeBlink
+        headMovement
+        style={{
+          width: "400px",
+          height: "400px",
+          backgroundColor: "orange",
+          borderRadius: "100%",
+        }}
+        url={
+          "https://d1a370nemizbjq.cloudfront.net/264909db-a426-41ee-89f6-607b5e0ba6aa.glb"
+        }
+      />
     </div>
   );
 }
